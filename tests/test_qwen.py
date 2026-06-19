63910f903752e860b4772b2b4cff2bfde8aa97c0
Artessay <rihong.edu@outlook.com>
2026-06-19 16:34:04 +0800

    A runnable version

--- tests/test_qwen.py
+++ tests/test_qwen.py
@@ -4,8 +4,11 @@ from openai import OpenAI
 client = OpenAI(
     api_key="EMPTY",
     base_url="http://localhost:8000/v1",
-    timeout=3600
 )
+
+# model_name = "qwen3.5-9b"
+model_name = "qwen3.5-122b-a10b"
+# model_name = "qwen3.5-397b-a17b-int4"
 
 messages = [
     {
@@ -16,9 +19,8 @@ messages = [
 
 start = time.time()
 response = client.chat.completions.create(
-    model="qwen3.5-9b",
+    model=model_name,
     messages=messages,
-    max_tokens=32768,
 )
 print(f"Response costs: {time.time() - start:.2f}s")
 print(f"Generated text: {response.choices[0].message.content}")
